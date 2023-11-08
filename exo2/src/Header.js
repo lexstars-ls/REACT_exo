23dc2fa2f04d139b2629918f7958d2c423afedc7
Sisovath lucas <lucas.sisovath@egs.school>
2023-11-08 10:37:44 +0100

    add variable

--- exo2/src/Header.js
+++ exo2/src/Header.js
@@ -1,16 +1,20 @@
-const yourName = true;
+
 
 function Header() {
+    const yourName = true;
+    const itemInCart = 2;
+
     return (
         <header>
         <h1>Header</h1>
         <nav>
             <ul>
+                {yourName ? <li>Bonjour Lucas</li> : <li>Veuillez vous connecter</li>}
                 <li>Accueil</li>
                 <li>Home</li>                
                 <li>Contact</li>
-
-                {yourName ? <li>Bonjour Lucas</li> : <li>Veuillez vous connecter</li>}
+                {itemInCart >0? <li> votre panier contient :{itemInCart}</li> : <li>Pas d'item aouter</li>}
+               
             </ul>
         </nav>
         </header>
