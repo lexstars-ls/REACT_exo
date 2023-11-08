8edb1e47973fad27c3c8c8686146181e674495e6
Sisovath lucas <lucas.sisovath@egs.school>
2023-11-08 15:10:12 +0100

    ajt exo 5

--- exo2/src/Products.js
+++ exo2/src/Products.js
@@ -1,51 +1,57 @@
 function Products() {
     const productsFromDb = [
         {
-            title: "aspirateur" ,
-            price:20 ,
-            IsPublished:true,
+            title: "aspirateur",
+            price: 20,
+            isPublished: true,
         },
         {
-            title: "canne a peche" ,
-            price:150 ,
-            IsPublished: true,
+            title: "canne a peche",
+            price: 150,
+            isPublished: true,
         },
         {
-            title:"casque de mobilette",
-            price:80 ,
-            IsPublished: true,
+            title: "casque de mobilette",
+            price: 80,
+            isPublished: true,
         }
-     ]
+    ]
 
 
 
-    return (   
-         
-        <>     
-        {/* // je boucle mon tableau avec le param product  */}
-        {productsFromDb.map((product)=>{
-         return(
-            // je retourne un article avec un titre qui contient a chaque fois une var de mon tableau
-            <article>
-                <h2>{product.title}  </h2>
-                <li>{product.price} euros</li>
-            </article>
-         )
-        })}
-        <p>Mes Products</p>  
-        <ul>
-            <li>
-                Premier product
-            </li>
-            <li>
-                Second product
-            </li>
-            <li>
-                Troisième product 
-            </li>
-        </ul>   
-        </>  
+    return (
+
+        <>
+            {/* // je boucle mon tableau avec le param product  */}
+            {productsFromDb.map((product) => {
+                return (
+                    <>
+
+                        {product.isPublished && (
+                            <>
+                                <h2>{product.title}</h2>
+                                <p>{product.price}</p>
+                            </>
+                        )
+                        }
+                    </>
+                )
+            })}
+
+            <p>Mes Products</p>
+            <ul>
+                <li>
+                    Premier product
+                </li>
+                <li>
+                    Second product
+                </li>
+                <li>
+                    Troisième product
+                </li>
+            </ul>
+        </>
     )
-} 
+}
 
 export default Products;
