64a015b8484b065c23495442886be64f23cfed36
Sisovath lucas <lucas.sisovath@egs.school>
2023-11-08 11:37:57 +0100

    add obj

--- exo2/src/Header.js
+++ exo2/src/Header.js
@@ -4,11 +4,18 @@ function Header() {
     const yourName = true;
     const itemInCart = 2;
 
+   const userLog = {
+    lastname : "kiwi",
+    firstname: " francis",
+    job : "vendeur de fruit",
+   }
+
     return (
         <header>
         <h1>Header</h1>
         <nav>
             <ul>
+                {userLog ? <li> votre nom {userLog.lastname}, votre prénom {userLog.firstname} </li> : <li>Veuillez vous connecter</li>}
                 {yourName ? <li>Bonjour Lucas</li> : <li>Veuillez vous connecter</li>}
                 <li>Accueil</li>
                 <li>Home</li>                
