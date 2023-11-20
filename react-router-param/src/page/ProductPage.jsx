0dcf39a81315f12c781a3fd1377c7434fc17ad1d
Sisovath lucas <lucas.sisovath@egs.school>
2023-11-20 12:31:26 +0100

    add utiis

--- react-router-param/src/page/ProductPage.jsx
+++ react-router-param/src/page/ProductPage.jsx
@@ -1,91 +1,9 @@
 import { useParams } from "react-router-dom";
 import Header from "../component/Header";
-
+import { products } from "../utils/products-utils";
 function ProductPage() {
   const { id } = useParams();
 
-  const products = [
-    {
-      id: 1,
-      title: "micro-onde",
-      publicationDate: "2021-09-01",
-      price: 100,
-      description: "chauffe les aliments",
-      category: "cuisine",
-    },
-    {
-      id: 2,
-      title: "four",
-      publicationDate: "2021-10-01",
-      price: 150,
-      description: "chauffe les aliments",
-      category: "cuisine",
-    },
-    {
-      id: 3,
-      title: "lave-linge",
-      publicationDate: "2021-11-01",
-      price: 400,
-      description: "lave les linges",
-      category: "salle de bain",
-    },
-    {
-      id: 4,
-      title: "lave-vaisselle",
-      publicationDate: "2021-08-01",
-      price: 200,
-      description: "lave les vaisselles",
-      category: "salle de bain",
-    },
-    {
-      id: 5,
-      title: "aspirateur",
-      publicationDate: "2022-02-01",
-      price: 720,
-      description: "aspire les poussières",
-      category: "salle de bain",
-    },
-    {
-      id: 6,
-      title: "cuisinière",
-      publicationDate: "2022-12-01",
-      price: 350,
-      description: "cuit les aliments",
-      category: "cuisine",
-    },
-    {
-      id: 7,
-      title: "réfrigérateur",
-      publicationDate: "2022-03-01",
-      price: 600,
-      description: "refroidit les aliments",
-      category: "cuisine",
-    },
-    {
-      id: 8,
-      title: "congélateur",
-      publicationDate: "2022-04-01",
-      price: 450,
-      description: "congèle les aliments",
-      category: "cuisine",
-    },
-    {
-      id: 9,
-      title: "télévision",
-      publicationDate: "2022-10-10",
-      price: 700,
-      description: "affiche des images",
-      category: "salon",
-    },
-    {
-      id: 10,
-      title: "ordinateur",
-      publicationDate: "2022-06-01",
-      price: 1000,
-      description: "affiche des images",
-      category: "salon",
-    },
-  ];
 
   // permet de convertir le type de id (chaine de caractères)
   // car récupérée dans l'url
@@ -94,7 +12,7 @@ function ProductPage() {
   // et comparer avec triple égal (strict)
   // plutôt que laisser JS faire la conversion et comparer avec double égal
   const idInt = parseInt(id);
-
+// 
   const productFound = products.find((product) => {
     return product.id === idInt;
   });
